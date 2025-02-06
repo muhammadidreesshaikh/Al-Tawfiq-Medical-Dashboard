a552be3a22f7965d1d08a5dfa8d1c9e7c9a78668
Muhammad Awais <awaisshaikh94@gmail.com>
2025-02-06 20:15:41 +0500

    relevant info done

--- src/app/components/relevant-information/relevant-information.component.ts
+++ src/app/components/relevant-information/relevant-information.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { TablerIconsModule } from 'angular-tabler-icons';
 import { MaterialModule } from 'src/app/material.module';
+
+interface IInformation {
+  name: string;
+  url: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-relevant-information',
-  imports: [ MaterialModule ],
+  imports: [ MaterialModule, TablerIconsModule ],
   templateUrl: './relevant-information.component.html',
   styleUrl: './relevant-information.component.scss'
 })
 export class RelevantInformationComponent {
-
+  informations: IInformation[] = [
+    {
+      name: 'Rulestream Academy',
+      url: '#',
+      icon: 'certificate'
+    },
+    {
+      name: 'NDMO Framework',
+      url: '#',
+      icon: 'stack-backward'
+    },
+    {
+      name: 'Internal Policies',
+      url: '#',
+      icon: 'lock-access'
+    }
+  ]
 }
